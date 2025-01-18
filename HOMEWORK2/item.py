4cfdcc7aef91bff38929a6ed95695414d7b973ef
juliamakareva <julia.makareva@gmail.com>
2025-01-18 12:57:08 +0100

    Sending my code to GITHUB

--- HOMEWORK2/item.py
+++ HOMEWORK2/item.py
@@ -1,7 +1,10 @@
+from typing import Any
+
+
 class Item:
     _id = 0  # counter for unique ID's
 
-    def __init__(self, name, description, quantity=0, dispatch_time=None, _tags=None, _cost=None):
+    def __init__(self, name, description, quantity=0, dispatch_time=None, _tags=None, _cost: float = None):
         Item._id += 1
         self.id = Item._id
         self.name = name
@@ -31,24 +34,41 @@ class Item:
         if tag not in self._tags:
             self._tags.append(tag)
 
+    def add_tags(self, tags):
+        """работает не с одним тегом а сразу с контейнером с несколькими тегами."""
+        for tag in tags:
+            if tag not in self._tags:
+                self._tags.append(tag)
+
     def rm_tag(self, tag: str):
         if tag in self._tags:
             self._tags.remove(tag)
 
-    def is_tagged(self,tags):
+    def rm_tags(self, tags):
+        """работает не с одним тегом а сразу с контейнером с несколькими тегами."""
+        for tag in tags:
+            if tag in self._tags:
+                self._tags.remove(tag)
+
+    def is_tagged(self, tags):
         "this f checks if all tags from the list are assigned to the item"
         for tag in tags:
             if tag not in self._tags:
                 return False
         return True
 
-
     def __len__(self):
         return len(self._tags)  # this f returns the number of tags
 
-    def set_cost(self):
-        self._cost = _cost
-
-    def get_cost(self):
+    @property
+    def cost(self) -> float | None:
         return self._cost
 
+    @cost.setter
+    def cost(self, new_cost):
+        if new_cost < 0:
+            raise ValueError("The cost cannot be negative")
+        self._cost = new_cost
+
+    def copy_item(self):
+        return Item(self.name, self.description, self.quantity, self.dispatch_time, self._tags[:], self._cost)
